090556f5d75c7c3bdb73721bd19860f7768ca6d8
Jaime Bernardo <jaime@janeasystems.com>
2018-04-05 22:44:25 +0100

    cordova-angular: add tests to sample app
    Adds code to handle app pause and resume.
    Adds code to test multiple message listeners, listener removal and
    using a writable path from nodejs side.

--- cordova-angularjs/src/client/app/app.js
+++ cordova-angularjs/src/client/app/app.js
@@ -10,7 +10,7 @@ angular
       url = url || 'http://localhost:8081';
       return io(url, { reconnection: true });
   }
-  
+
   var theInstance,
       foregroundDeferred,
       pendingInstanceDeferred;
@@ -76,10 +76,20 @@ function($rootScope, $http, $timeout, theSocket) {
     $rootScope.debugMessages = [];
 
     var counter = 0;
-    function appendToLog(msg) {
+    function appendToLog(msg, forceDigest) {
         console.log(msg);
         counter++;
         $rootScope.debugMessages.push(counter + ': ' + msg);
+        if(forceDigest) {
+            // Force angular scope update when out of scope, so the messages will appear as soon as possible.
+            // Mostly used for callback environments, since it will cause an error if there's a digest already in progress.
+            $rootScope.$digest();
+        }
+    }
+    function clearLog() {
+        console.log("Log was cleared.");
+        counter = 0;
+        $rootScope.debugMessages = [];
     }
 
     var app = {
@@ -95,7 +105,7 @@ function($rootScope, $http, $timeout, theSocket) {
                 $timeout(checkHttp, 2000);
             }
         },
-    
+
         onDeviceReady: function() {
             this.receivedEvent('deviceready');
             startNodeProject();
@@ -103,6 +113,7 @@ function($rootScope, $http, $timeout, theSocket) {
 
         onDevicePause: function() {
             this.receivedEvent('pause');
+            stopSocket();
             theSocket.destroy();
         },
 
@@ -110,34 +121,43 @@ function($rootScope, $http, $timeout, theSocket) {
             this.receivedEvent('resume');
             checkHttp();
         },
-    
+
         receivedEvent: function(id) {
-            appendToLog('AnguarJs Received Event: ' + id);
+            appendToLog('AngularJS Received Event: ' + id, true);
         }
     };
-        
+
     app.initialize();
 
     function channelListener(msg) {
-        appendToLog('[AngularJs Cordova] received: ' + msg);
-        checkHttp();
+        appendToLog('[AngularJS Cordova] received: ' + msg, true);
     }
-    
+
     function startNodeProject() {
         nodejs.channel.setListener(channelListener);
+
+        nodejs.channel.on('angular-log', (msg) => {
+            // For receiving log requests from node.
+            appendToLog("node-log: " + msg, true);
+        });
+
+        nodejs.channel.on('started', (msg) => {
+            appendToLog("Node sent the 'started' event, with this message: " + msg, true);
+            checkHttp();
+        });
+
         nodejs.start('main.js',
         function(err) {
             if (err) {
-                appendToLog('AngularJs, failed NodeJs engine' + err);
+                appendToLog('AngularJS, failed NodeJs engine' + err, true);
             } else {
-                appendToLog('AngularJs, started NodeJs engine');
-                nodejs.channel.send('Hello from AngularJs Cordova!');
+                appendToLog('AngularJS, started NodeJs engine', true);
             }
         });
     }
 
     function checkHttp() {
-        appendToLog('[AngularJs Cordova] checking HTTP API');
+        appendToLog('[AngularJS Cordova] checking HTTP API');
         $http.get('http://localhost:8081')
         .then(function(response) {
             $rootScope.message = JSON.stringify(response.data);
@@ -145,37 +165,107 @@ function($rootScope, $http, $timeout, theSocket) {
         })
         .catch(function(err) {
             $rootScope.message = 'HTTP err:' + JSON.stringify(err);
-            appendToLog('HTTP err: ' + JSON.stringify(err));
+            appendToLog('HTTP err: ' + JSON.stringify(err), true);
         });
     }
 
     function startSocket() {
-        appendToLog('[AngularJs Cordova] starting socket via API');
+        appendToLog('[AngularJS Cordova] starting socket via API');
         theSocket.getInstance()
         .then(function (socket) {
             socket.on('data_update', function(data) {
-                appendToLog('AngularJs got socket data: ' + data);
-                $rootScope.$digest();
+                appendToLog('AngularJS got socket data: ' + data, true);
             });
             socket.emit('start_data_updates', {});
         }).catch(function(err) {
-            appendToLog('AngularJs Socket Error: ' + err);
+            appendToLog('AngularJS Socket Error: ' + err, true);
         });
     }
 
     function stopSocket() {
-        appendToLog('[AngularJs Cordova] stopping socket via API');
+        appendToLog('[AngularJS Cordova] stopping socket via API');
         theSocket.getInstance()
         .then(function (socket) {
             socket.emit('stop_data_updates', {});
             socket.off();
         }).catch(function(err) {
-            appendToLog('AngularJs Socket Error: ' + err);
+            appendToLog('AngularJS Socket Error: ' + err, true);
         });
+    }
+
+    function doFileWrite() {
+        nodejs.channel.removeAllListeners('test-file-saved');
+        var randomData=Math.random().toString(36).substring(7);
+        appendToLog('Will tell nodejs to write this to a file: '+ randomData);
+        nodejs.channel.on('test-file-saved', (msg) => {
+            appendToLog('nodejs says it saved data in :' + msg, true);
+
+            window.resolveLocalFileSystemURL('file://' + msg,
+                function(fileEntry) {
+
+                    fileEntry.file( (file) => {
+                        var reader = new FileReader();
+                        reader.onloadend = function() {
+                            appendToLog('Read ' + this.result + ' Expected ' + randomData, true);
+                        };
+                        reader.readAsText(file);
+                    });
+
+                }
+                , function(error) {
+                    appendToLog('Error while getting ' + msg + ' -> ' + JSON.stringify(error), true);
+                }
+            );
+        });
+        nodejs.channel.post('test-file', randomData);
+    }
+
+    function sendEcho() {
+        // Sends echo request to nodeJS.
+        nodejs.channel.post('node-echo', 'Hello from AngularJS Cordova!');
+    }
+
+    function toggleEcho() {
+        // Toggles echo on and off on the nodeJS side.
+        nodejs.channel.post('control', {
+            action: 'toggle-event-listeners',
+            eventName: 'node-echo'
+        });
+    }
+
+    let additionalEchoListeners = [];
+    let createdListenerCount = 1;
+
+    function addAnotherEchoListener() {
+        // Adds another listener to message events and saves it for later removal.
+        let newListener = ( function() {
+            let thisListenerId = createdListenerCount++;
+            return (msg) => { appendToLog('Another ' + thisListenerId + ' : ' + msg, true); };
+        })();
+        nodejs.channel.on('message', newListener);
+        additionalEchoListeners.push(newListener);
+        appendToLog('Another listener has been added. Test it with Echo.');
+    }
+
+    function removeAnotherEchoListener() {
+        // Removes one of the "another" listeners from the message events.
+        let listenerToRemove = additionalEchoListeners.shift();
+        if (typeof listenerToRemove === "undefined") {
+            appendToLog('No more listeners to remove.');
+        } else {
+            nodejs.channel.removeListener('message', listenerToRemove);
+            appendToLog('Removed another listener.');
+        }
     }
 
     $rootScope.doHttp  = checkHttp;
     $rootScope.doStart = startSocket;
     $rootScope.doStop  = stopSocket;
+    $rootScope.clearLog = clearLog;
+    $rootScope.sendEcho = sendEcho;
+    $rootScope.toggleEcho = toggleEcho;
+    $rootScope.doFileWrite = doFileWrite;
+    $rootScope.addAnotherEchoListener = addAnotherEchoListener;
+    $rootScope.removeAnotherEchoListener = removeAnotherEchoListener;
 }
 ]);
--- cordova-angularjs/src/server/main.js
+++ cordova-angularjs/src/server/main.js
@@ -7,14 +7,8 @@ if ((os.platform() === "ios") ||  (os.platform() === "android")) {
     isMobile = true;
 }
 
-if (isMobile) {
-    const cordova = require('cordova-bridge');
-
-    cordova.channel.on('message', (msg) => { 
-        console.log('[NodeJS Mobile] received:', msg); 
-        cordova.channel.send('NodeJS replying to to cordova with this message: ' + msg);
-    });
-}
+const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
 const app = express();
@@ -51,7 +45,7 @@ io.on('connection', (newSocket) => {
             clearInterval(timer);
             timer = null;
         }
-    });    
+    });
 
     newSocket.on('error', (err) => {
         console.log('Socket.io: error: ' + err);
@@ -63,6 +57,84 @@ io.on('connection', (newSocket) => {
     });
 });
 
-server.listen(8081, () => {
-    console.log('NodeJS listening on 8081');
-});
+function server_start() {
+    server.listen(8081, () => {
+        console.log('NodeJS listening on 8081');
+    });
+}
+
+server_start();
+if (isMobile) {
+    const cordova = require('cordova-bridge');
+
+    cordova.app.on('pause', () => {
+        server.close();
+    });
+
+    cordova.app.on('resume', () => {
+        server_start();
+    });
+
+    let listenersBackup = {};
+    function backupListenersOfEvent(eventName) {
+        // Copies the current listeners of an event to the backup.
+        listenersBackup[eventName] = cordova.channel.listeners(eventName);
+    }
+    function restoreListenersOfEvent(eventName) {
+        // Restores the listeners of an event from the backup.
+        var backedUpListeners = listenersBackup[eventName];
+        if (Array.isArray(backedUpListeners)) {
+            for(var i = 0; i < backedUpListeners.length; i++) {
+                cordova.channel.addListener(eventName,backedUpListeners[i]);
+            }
+        }
+    }
+    function toggleListenersOfEvent(eventName) {
+        // If there are listeners for a event, backup and remove them.
+        // Otherwise, try to restore a backup.
+        if (cordova.channel.listenerCount(eventName) > 0) {
+            backupListenersOfEvent(eventName);
+            cordova.channel.removeAllListeners(eventName);
+        } else {
+            restoreListenersOfEvent(eventName);
+        }
+    }
+
+    cordova.channel.on('control', (msg) => {
+        // Will listen for different control message objects from cordova.
+        if (typeof msg === 'object') {
+            if(msg && msg.action) {
+                switch(msg.action) {
+                    case 'toggle-event-listeners':
+                        // Will be used to toggle events on and off.
+                        if (typeof msg.eventName === "string") {
+                            toggleListenersOfEvent(msg.eventName);
+                        } else {
+                            cordova.channel.post('angular-log', "control channel received incorrect channel name: " + JSON.stringify(msg));
+                        }
+                        break;
+                    default:
+                        cordova.channel.post('angular-log', "control channel received unknown action: " + JSON.stringify(msg));
+                }
+            } else {
+                cordova.channel.post('angular-log', "control channel received an object without an action: " + JSON.stringify(msg));
+            }
+        } else {
+            cordova.channel.post('angular-log', "control channel didn't receive an object.");
+        }
+    });
+
+    cordova.channel.on('node-echo', (msg) => {
+        console.log('[NodeJS Mobile] received:', msg);
+        cordova.channel.send('NodeJS replying to to cordova with this message: ' + msg);
+    });
+
+    cordova.channel.on('test-file', (msg) => {
+        var writablePath = path.join(cordova.app.datadir(), "writefile.txt");
+        fs.writeFile(writablePath, msg, () => {
+            cordova.channel.post('test-file-saved', writablePath);
+        });
+    });
+
+    cordova.channel.post('started' , "Hi, cordova! It's node! I've started. Mind checking that HTTP?");
+}
