0c4c03b099369f331a21f8c337c8753c7eb9fe51
Rinyuaru Kun <23364198+Rinyuaru-Kun@users.noreply.github.com>
2018-05-02 11:58:42 +0200

    Removed unnecessary folders from git

--- desktop-and-mobile/cordova/plugins/nodejs-mobile-cordova/install/sample-project/www/nodejs-project/main.js
+++
@@ -1,6 +0,0 @@
-const cordova = require('cordova-bridge');
-
-cordova.channel.on('message', function (msg) { 
-  console.log('[node] received:', msg); 
-  cordova.channel.send('Replying to this message: ' + msg);
-});
