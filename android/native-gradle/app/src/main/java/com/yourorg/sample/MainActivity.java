49c09414a7120a980d3ff38b1e53f6e213e594c5
Arjun <76874556+1407arjun@users.noreply.github.com>
2021-09-15 19:58:22 +0530

    Change Integer object to int in native function

--- android/native-gradle/app/src/main/java/com/yourorg/sample/MainActivity.java
+++ android/native-gradle/app/src/main/java/com/yourorg/sample/MainActivity.java
@@ -80,5 +80,5 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native Integer startNodeWithArguments(String[] arguments);
+    public native int startNodeWithArguments(String[] arguments);
 }
