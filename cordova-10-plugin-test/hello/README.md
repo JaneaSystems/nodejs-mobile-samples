7dba8710de2b8f3dd354be79e9271ca7118fd4a0
jasonh <appleoxxo@gmail.com>
2021-11-10 15:14:31 +0800

    add java js

---
+++ cordova-10-plugin-test/hello/README.md
@@ -0,0 +1,19 @@
+# scripts
+
+`npm install`
+
+`npx cordova plugin  add https://github.com/JaosnHsieh/cordova-plugin-fibo`
+
+`npx cordova run android`
+
+`adb logcat`
+
+
+Fibonacci calculation in  java faster than javascript.
+
+![Screen capture animation](./20211110-demo.gif)
+
+
+## TODO
+
+add iOS demo
--- cordova-10-plugin-test/hello/www/js/index.js
+++ cordova-10-plugin-test/hello/www/js/index.js
@@ -19,11 +19,44 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+console.log('$ javascript testing.....')
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
+    console.log(`$ onDeviceReady`);
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    document.getElementById("getFromJavaBtn").onclick = ()=>{
+        const start = new Date();
+        console.log(`$ onclick #getFromJavaBtn`)
+        const fiboInput = document.getElementById('fibo-input').value;
+        console.log(`$ fiboInput ${fiboInput}`)
+        window.plugins.fibo.get(fiboInput,function onSuccess(value){
+            const duration = new  Date().getTime()-start.getTime();
+            document.getElementById('duration').textContent =  `duration ${duration} ms`;
+            console.log(`$ window.plugins.fibo.get(${fiboInput}) onSuccess value`,value);
+            document.getElementById('fibo-java-result').textContent = `${value}`;
+        },function onFailed(err){
+            console.error(`$ err`,err)
+        });
+    }
+
+    document.getElementById("getFromJsBtn").onclick = ()=>{
+        console.log(`$ onclick #getFromJsBtn`)
+        const fiboInput = document.getElementById('fibo-input').value;
+        console.log(`$ fiboInput ${fiboInput}`)
+        const start = new Date();
+        const value = fibo(parseInt(fiboInput));
+        const duration = new  Date().getTime()-start.getTime();
+        document.getElementById('duration').textContent =  `duration ${duration} ms`;
+        document.getElementById('fibo-js-result').textContent = `${value}`;
+
+        function fibo(n){
+            if(n<=1){
+                return n;
+            }
+            return fibo(n-1)+fibo(n-2);
+        }
+    }
 }
